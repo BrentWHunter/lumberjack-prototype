7b460026c468fa7a44e937a2774c81c929f336fc
Brent Hunter <bh@othelloventures.com>
2019-03-20 13:37:06 -0700

    Change loading popup to use lumberjack gif.

--- source/components/loadingpopup.js
+++ source/components/loadingpopup.js
@@ -5,7 +5,8 @@ enyo.kind({
 
 	subComponents: [
 		{kind: "enyo.FittableRows", style: "max-width: 510px; padding: 10px; text-align: center;", components: [
-			{kind: "onyx.Spinner"},
+			//{kind: "onyx.Spinner"},
+			{kind: "enyo.Image", src: "assets/lumberjack.gif"},
 			{name: "loadingMessage", style: "text-align: center; font-size: 24px; font-weight: bold; margin-top: 10px;", content: $L("Loading")}
 		]}
 	],
